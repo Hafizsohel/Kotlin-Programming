629d0258407077c8e4d9caf334f0433aa2bef93d
Hafizur Rahman <sohel.hafizurrahman@gmail.com>
2024-05-16 14:59:52 +0600

    2nd day

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>){
-    println("Hello World!")
+   var String="Hello Kotlin"
+    var number=10
+    var myFloat=0.10
+
+    var name:String
+    name="Steve"
+
+   display(name)
+}
+
+fun display(name:String){
+    print(name)
 }
